508baf2a90712bcc967b1e3de10a502c96e5b9b8
kajugadaniels <kajugadaniels@gmail.com>
2025-10-28 18:01:03 +0200

    Add ActivityOverview component

---
+++ src/components/dashboard/DentalHealthOverview.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DentalHealthOverview = () => {
+    return (
+        <div>
+            Dental Health Overview Component
+        </div>
+    )
+}
+
+export default DentalHealthOverview
