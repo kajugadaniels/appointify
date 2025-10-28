8caa64d24c13aab1ec405582c895f31254e9b4a9
kajugadaniels <kajugadaniels@gmail.com>
2025-10-28 18:02:30 +0200

    Add next appointment component

---
+++ src/components/dashboard/NextAppointment.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NextAppointment = () => {
+    return (
+        <div>
+            Next Appointment Component
+        </div>
+    )
+}
+
+export default NextAppointment
