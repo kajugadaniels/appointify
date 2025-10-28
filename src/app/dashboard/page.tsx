8b4886315b5413bd4b2d0ac0188b85d0a420495d
kajugadaniels <kajugadaniels@gmail.com>
2025-10-28 18:07:41 +0200

    Design dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainActions from "@/components/dashboard/MainActions";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
+import Navbar from "@/components/Navbar";
 
-const Dashboard = () => {
+function DashboardPage() {
     return (
-        <div>
-            Welcome User
-        </div>
-    )
-}
+        <>
+            <Navbar />
 
-export default Dashboard
+            <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+                <WelcomeSection />
+                <MainActions />
+                <ActivityOverview />
+            </div>
+        </>
+    );
+}
+export default DashboardPage;
