90cc04e4365e3a8309399f75746c16e5930edd7a
kajugadaniels <kajugadaniels@gmail.com>
2025-10-29 17:52:55 +0200

    Redirect logged in user to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
-import CTA from '@/components/landing/CTA'
-import Footer from '@/components/landing/Footer'
-import Header from '@/components/landing/Header'
-import Hero from '@/components/landing/Hero'
-import HowItWorks from '@/components/landing/HowItWorks'
-import PricingSection from '@/components/landing/PricingSection'
-import WhatToAsk from '@/components/landing/WhatToAsk'
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
+import { syncUser } from "@/lib/actions/user";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
     const user = await currentUser();
 
-    if (user) redirect('/dashboard');
+    // the best way of syncing => webhooks
+    await syncUser();
+
+    // redirect auth user to dashboard
+    if (user) redirect("/dashboard");
 
     return (
-        <div className='min-h-screen bg-background'>
+        <div className="min-h-screen bg-background">
             <Header />
             <Hero />
             <HowItWorks />
@@ -23,5 +28,5 @@ export default async function Home() {
             <CTA />
             <Footer />
         </div>
-    )
+    );
 }
