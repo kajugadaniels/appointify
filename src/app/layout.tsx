56ec5b874b30d5ecf0ee6f4b0be922e605f658c6
kajugadaniels <kajugadaniels@gmail.com>
2025-10-29 22:42:00 +0200

    Setup TanStackProvider in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import UserSync from "@/components/UserSync";
+import TanStackProvider from "@/components/providers/TanStackProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -15,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Appointify - AI Powered Dental Assistant",
+    title: "DentWise - AI Powered Dental Assistant",
     description:
         "Get instant dental advice through voice calls with our AI assistant. Avaiable 24/7.",
 };
@@ -26,15 +27,25 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <ClerkProvider>
-            <html lang="en">
-                <body
-                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                >
-                    <UserSync />
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
+        <TanStackProvider>
+            <ClerkProvider
+                appearance={{
+                    variables: {
+                        colorPrimary: "#e78a53",
+                        colorBackground: "#f3f4f6",
+                        colorText: "#111827",
+                        colorTextSecondary: "#6b7280",
+                        colorInputBackground: "#f3f4f6",
+                    },
+                }}
+            >
+                <html lang="en">
+                    <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+                        <Toaster />
+                        {children}
+                    </body>
+                </html>
+            </ClerkProvider>
+        </TanStackProvider>
     );
 }
