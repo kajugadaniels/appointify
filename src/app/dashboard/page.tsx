ea3f257b18622e7f90655b6ed4ea5d35d38c213d
kajugadaniels <kajugadaniels@gmail.com>
2025-10-29 17:56:24 +0200

    Add main action to user dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import ActivityOverview from '@/components/dashboard/ActivityOverview'
+import MainActions from '@/components/dashboard/MainActions'
 import WelcomeSection from '@/components/dashboard/WelcomeSection'
 import Navbar from '@/components/Navbar'
 
@@ -8,6 +10,8 @@ const Dashboard = () => {
 
             <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
                 <WelcomeSection />
+                <MainActions />
+                <ActivityOverview />
             </div>
         </>
     )
