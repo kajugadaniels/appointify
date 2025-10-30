ef07a608e79e479d780318ff8ff6a8c6a50f834c
kajugadaniels <kajugadaniels@gmail.com>
2025-10-30 02:53:58 +0200

    New setup with eslint

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatar.iran.liara.run",
-      },
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-      },
-    ],
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
